1fc8ab91f7f35121e1874064c51e0774ab59c9ba
Mateus Franco <msfranco93@gmail.com>
2022-12-18 19:15:30 -0300

    Adjusting text box background

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -20,15 +20,16 @@ function Skill({ skill,directionLeft }: Props) {
                 opacity: 1,
                 x: 0 
             }}
-            className='w-10 h-10 rounded-full border-2 border-[#E5383B] object-cover 
-            filter group-hover:grayscale trasition duration-300 md:h-16 md:w-16 xl:h-20 xl:w-20 bg-white'
+            className='rounded-full border-2 border-[#E5383B] object-cover 
+            filter group-hover:grayscale trasition duration-300 
+            w-10 h-10 md:h-16 md:w-16 xl:h-20 xl:w-20 bg-white'
             src={skill.image.url}
         />
         <div className='absolute opacity-0  rounded-full z-0
-        group-hover:opacity-90 group-hover:bg-white transition duration-100 ease-in-out
-        h-10 w-10 md:h-20 md:w-20'>
+        group-hover:opacity-90 group-hover:bg-white transition duration-200 ease-in-out
+        w-10 h-10 md:h-16 md:w-16 xl:h-20 xl:w-20'>
             <div className='flex items-center justify-center h-full'>
-                <p className='text-sm font-bold text-black opacity-100 text-center'>{skill?.name}</p>
+                <p className='text-xs font-bold text-black opacity-100 text-center bg-white rounded opacity-90 noselect'>{skill?.name}</p>
             </div>
         </div>
     </div>
