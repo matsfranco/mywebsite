7e5df0baab1f22c2ce44fb486af85de5ac6c8737
Mateus Franco <msfranco93@gmail.com>
2022-12-18 23:01:51 -0300

    adjusting Skill image size for mobile and sm screens

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -22,12 +22,12 @@ function Skill({ skill,directionLeft }: Props) {
             }}
             className='rounded-full border-2 border-[#E5383B] object-cover 
             filter group-hover:grayscale trasition duration-300 
-            w-10 h-10 md:h-16 md:w-16 xl:h-20 xl:w-20 bg-white'
+            w-14 h-14 md:h-16 md:w-16 xl:h-20 xl:w-20 bg-white'
             src={skill.image.url}
         />
         <div className='absolute opacity-0  rounded-full z-0
         group-hover:opacity-90 group-hover:bg-white transition duration-200 ease-in-out
-        w-10 h-10 md:h-16 md:w-16 xl:h-20 xl:w-20'>
+        w-14 h-14 md:h-16 md:w-16 xl:h-20 xl:w-20'>
             <div className='flex items-center justify-center h-full'>
                 <p className='text-xs font-bold text-black opacity-100 text-center bg-white rounded opacity-90 noselect'>{skill?.name}</p>
             </div>
